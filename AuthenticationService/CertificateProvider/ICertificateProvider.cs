5f727af8399e8f16d1866d030eca0db83723fd6f
Lars Nysom <LANY@ucn.dk>
2022-11-24 14:15:57 +0100

    Clean up

--- AuthenticationService/CertificateProvider/ICertificateProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace AuthenticationService.CertificateProvider
-{
-    public interface ICertificateProvider
-    {
-        X509Certificate2 CreateCertificate();
-    }
-}
