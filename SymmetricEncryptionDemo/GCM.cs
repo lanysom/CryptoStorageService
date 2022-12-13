3baa59d4daff1737090ce4e897b844cc3df9df52
Lars Nysom <LANY@ucn.dk>
2022-12-13 08:14:05 +0100

    Added cbc_MAC and GCM demo

---
+++ SymmetricEncryptionDemo/GCM.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricEncryptionDemo
+{
+    internal class GCM
+    {
+        public static (string, string, string, string) Encrypt(string message)
+        {
+            // the key length must be 16, 24, or 32 bytes 
+            byte[] key = new byte[32];
+            RandomNumberGenerator.Fill(key);
+
+            // the supported nonce sizes is 12 bytes 
+            byte[] nonce = new byte[AesGcm.NonceByteSizes.MaxSize];
+            RandomNumberGenerator.Fill(nonce);
+
+            // the supported tag sizes are 12, 13, 14, 15, or 15 bytes 
+            byte[] tag = new byte[AesGcm.TagByteSizes.MaxSize];
+
+            // the plaintext parameter and the ciphertext must have the same length.
+            byte[] plaintext = Encoding.UTF8.GetBytes(message);
+            byte[] ciphertext = new byte[plaintext.Length];
+
+            using AesGcm aesGcm = new(key);
+            aesGcm.Encrypt(nonce, plaintext, ciphertext, tag);
+
+            string encryptedMessage = Convert.ToBase64String(ciphertext);
+            string generatedTag = Convert.ToBase64String(tag);
+            string generatedNonce = Convert.ToBase64String(nonce);
+            string generatedKey = Convert.ToBase64String(key);
+
+            return (generatedKey, generatedNonce, generatedTag, encryptedMessage);
+        }
+
+        public static string Decrypt(string keyString, string nonceString, string tagString, string ciphertextString)
+        {
+            byte[] key = Convert.FromBase64String(keyString);
+            byte[] nonce = Convert.FromBase64String(nonceString);
+            byte[] tag = Convert.FromBase64String(tagString);
+            byte[] ciphertext = Convert.FromBase64String(ciphertextString);
+
+            // the plaintext and the ciphertext must have the same length.
+            byte[] plaintext = new byte[ciphertext.Length];
+
+            using AesGcm aesGcm = new(key);
+            aesGcm.Decrypt(nonce, ciphertext, tag, plaintext);
+
+            return Encoding.UTF8.GetString(plaintext);
+        }
+    }
+}
---
+++ SymmetricEncryptionDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using SymmetricEncryptionDemo;
+using System.Security.Cryptography;
+using System.Text;
+
+string plaintext = "The workshop instructor is a cryptographic genius";
+
+(string key, string nonce, string tag, string ciphertext) = GCM.Encrypt(plaintext);
+
+Console.WriteLine($"Plaintext: {plaintext}");
+Console.WriteLine($"CipherText: {ciphertext}");
+Console.WriteLine($"Key: {key}");
+Console.WriteLine($"Nonce: {nonce}");
+Console.WriteLine($"Tag: {tag}");
+
+Console.WriteLine($"Decrypted ciphertext: {GCM.Decrypt(key, nonce, tag, ciphertext)}");
